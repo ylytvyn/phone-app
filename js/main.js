62dc2c6b4ae8efbb5c7f25075745f3613bb83fb8
Yuliya <juliankawonder@gmail.com>
2018-09-07 12:50:25 +0300

    add and delete functionality

--- js/main.js
+++ js/main.js
@@ -4,15 +4,42 @@
 	$(document).ready(function() {
 		var BASE_URL = 'http://localhost:3000',
 			$preloader = $('.preload'),
-			$list = $('.phones-list');
+			$list = $('.phones-list'),
+			$createBtn = $('.create');
 
 		$preloader.hide();
 		loadPhones();
 
-		// Code
+		// Events
 		$('.load').click(function() {
 			loadPhones();
 		});
+
+		$('.create').click(function() {
+			var newPhone = {};
+
+			$('.field').each(function() {
+				newPhone[this.name] = this.value;
+
+				this.value = '';
+			});
+
+			newPhone.age = Number($('.phones-list li').length);
+
+			addPhone(newPhone);
+		});
+
+		$('.field').on('blur', function() {
+
+			if (this.value !== '') {
+				$createBtn.prop('disabled', false);
+			} else {
+				$createBtn.prop('disabled', true);
+			}
+
+		});
+
+		// Functions
 
 		function loadPhones() {
 			$list.html('');
@@ -30,10 +57,15 @@
 										<div>
 											<h2>${data[i].name}</h2>
 											<p>${data[i].snippet}</p>
+											<i class="delete" data-id="${data[i].id}">Delete</i>
 										</div>
 									</li>`;
 
 						$list.append(elem);
+					});
+
+					$('.delete').click(function() {
+						deletePhone($(this).data('id'));
 					});
 				},
 				statusCode: {
@@ -48,6 +80,38 @@
 					$preloader.hide();
 				}, 500);
 
+			});
+		}
+
+		function deletePhone(id) {
+			var answer = confirm('Are you sure?');
+
+			if (answer) {
+
+				$.ajax({
+					type: 'DELETE',
+					url: `${BASE_URL}/phones/${id}`,
+					beforeSend: () => {
+						$preloader.show();
+					}
+				}).then(() => {
+					loadPhones();
+				});
+
+			}
+		}
+
+		function addPhone(data) {
+			$.ajax({
+				type: 'POST',
+				url: `${BASE_URL}/phones`,
+				data: data,
+				beforeSend: () => {
+					$preloader.show();
+				}
+			}).then(() => {
+				loadPhones();
+				$createBtn.prop('disabled', true);
 			});
 		}
 	});
